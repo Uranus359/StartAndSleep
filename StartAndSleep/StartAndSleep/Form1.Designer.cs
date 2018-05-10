532df424ea6d505e65943937b02b4aaa95e208e5
Uranus359 <38375886+Uranus359@users.noreply.github.com>
2018-05-10 10:20:27 +1000

    Add files via upload

---
+++ StartAndSleep/StartAndSleep/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace StartAndSleep
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(343, 382);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(361, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Пауза процесса";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(361, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 54);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Возобновление процесса";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(343, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Обновить список";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(361, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(163, 54);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Возобновить и закрыть процесс";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 434);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ StartAndSleep/StartAndSleep/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace StartAndSleep
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Process> apps = new Dictionary<string, Process>();
+        Process curProc;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FindProcesses();
+            listBox1.SelectedIndex = 0;
+            button1.Click += Button1_Click;
+            button2.Click += Button2_Click;
+            button3.Click += Button3_Click;
+            button4.Click += Button4_Click;
+            LockGUI(LockEvent.Before);
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            LockGUI(LockEvent.Resume);
+            WinAPI.ResumeProcess(curProc.Id);
+            WinAPI.ShowWindow(curProc.MainWindowHandle, WinAPI.SW_SHOW);
+            curProc.Kill();
+            apps.Clear();
+            listBox1.Items.Clear();
+            FindProcesses();
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            apps.Clear();
+            listBox1.Items.Clear();
+            FindProcesses();
+            listBox1.SelectedIndex = 0;
+        }
+
+        private void FindProcesses()
+        {
+            var processes = Process.GetProcesses().Where(p => p.MainWindowHandle != IntPtr.Zero);
+            foreach (var proc in processes)
+            {
+                var pname = $"{proc.ProcessName} [{proc.Id}]"; // without clones
+                listBox1.Items.Add(pname);
+                apps.Add(pname, proc);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            LockGUI(LockEvent.Suspend);
+            curProc = apps[listBox1.SelectedItem.ToString()];
+            WinAPI.ShowWindow(curProc.MainWindowHandle, WinAPI.SW_HIDE);
+            WinAPI.SuspendProcess(curProc.Id);
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            LockGUI(LockEvent.Resume);
+            WinAPI.ResumeProcess(curProc.Id);
+            WinAPI.ShowWindow(curProc.MainWindowHandle, WinAPI.SW_SHOW);
+        }
+
+        void LockGUI(LockEvent which)
+        {
+            if (which == LockEvent.Before)
+            {
+                button2.Enabled = false;
+                button4.Enabled = false;
+            }
+            else if (which == LockEvent.Suspend)
+            {
+                button1.Enabled = false;
+                listBox1.Enabled = false;
+                button3.Enabled = false;
+                button2.Enabled = true;
+                button4.Enabled = true;
+            }
+            else if (which == LockEvent.Resume)
+            {
+                button1.Enabled = true;
+                listBox1.Enabled = true;
+                button3.Enabled = true;
+                button2.Enabled = false;
+                button4.Enabled = false;
+            }
+        }
+    }
+
+    public enum LockEvent
+    {
+        Before,
+        Suspend,
+        Resume,
+    }
+
+    public static class WinAPI
+    {
+        public const int SW_HIDE = 0;
+        public const int SW_SHOW = 5;
+
+        [DllImport("User32")]
+        public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200)
+        }
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+        [DllImport("kernel32.dll")]
+        static extern uint SuspendThread(IntPtr hThread);
+        [DllImport("kernel32.dll")]
+        static extern int ResumeThread(IntPtr hThread);
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool CloseHandle(IntPtr handle);
+
+
+        public static void SuspendProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                SuspendThread(pOpenThread);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+
+        public static void ResumeProcess(int pid)
+        {
+            var process = Process.GetProcessById(pid);
+
+            if (process.ProcessName == string.Empty)
+                return;
+
+            foreach (ProcessThread pT in process.Threads)
+            {
+                IntPtr pOpenThread = OpenThread(ThreadAccess.SUSPEND_RESUME, false, (uint)pT.Id);
+
+                if (pOpenThread == IntPtr.Zero)
+                {
+                    continue;
+                }
+
+                var suspendCount = 0;
+                do
+                {
+                    suspendCount = ResumeThread(pOpenThread);
+                } while (suspendCount > 0);
+
+                CloseHandle(pOpenThread);
+            }
+        }
+    }
+}
